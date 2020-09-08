d2f05a572a8488caaab574cd9044cba292d0a445
simonesinceri <56643807+simonesinceri@users.noreply.github.com>
2020-09-08 23:17:20 +0200

    code smells test selenium

--- src/test/TestLoginSeleniumApi.java
+++ src/test/TestLoginSeleniumApi.java
@@ -19,22 +19,22 @@ public class TestLoginSeleniumApi {
 	@Test
 	public void TestLoginSelenium() {
 	
-		
+		String user ="simone";		
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://localhost:8080/FindITTT/");
 		driver.findElement(By.xpath("//*[@id=\"header\"]/ul/li[5]/a")).click();  //*[@id="box1"]/button
 		driver.findElement(By.xpath("//*[@id=\"box1\"]/button")).click();
 		
-		driver.findElement(By.xpath("//*[@id=\"box1\"]/form/input[1]")).sendKeys("simone");
-		driver.findElement(By.xpath("//*[@id=\"box1\"]/form/input[2]")).sendKeys("simone");
+		driver.findElement(By.xpath("//*[@id=\"box1\"]/form/input[1]")).sendKeys(user);
+		driver.findElement(By.xpath("//*[@id=\"box1\"]/form/input[2]")).sendKeys(user);
 		driver.findElement(By.xpath("//*[@id=\"box1\"]/form/input[3]")).click();
 		
 	
-		WebElement TxtBoxContent = driver.findElement(By.xpath("//*[@id=\"box1\"]/label[1]"));
+		WebElement txtBoxContent = driver.findElement(By.xpath("//*[@id=\"box1\"]/label[1]"));
 		
 
-		assertEquals("simone", TxtBoxContent.getText()); 
+		assertEquals(user, txtBoxContent.getText()); 
 		
 		driver.close();
 		
